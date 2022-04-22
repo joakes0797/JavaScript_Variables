79a79ef2bd9177f8e91616fa5ac81c5ae93b4eb0
Jill Oakes <jericho_rules@hotmail.com>
2022-04-22 12:22:20 -0400

    Complete Exercises 1, 2, 3

--- app.js
+++ app.js
@@ -4,7 +4,21 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Jill";
+let lastName = "Oakes";
+var age = "21";
 
-// Exercise 2
+// Exercise 2 using concatenation
+let fullName = firstName + " " + lastName;
+console.log("using concatenation: ");
+console.log(fullName);
+// Exercise 2 using template literal
+let fullName2 = `${firstName} ${lastName}`;
+console.log("using template literal:  ");
+console.log(fullName2);
 
 // Exercise 3
+let city = "Indianapolis";
+let pastime = "video games";
+let myStory = `Bonjour, my name is ${fullName}.  I live in in ${city}.  Enjoying ${pastime} is a hobby of mine.`;
+console.log(myStory);
